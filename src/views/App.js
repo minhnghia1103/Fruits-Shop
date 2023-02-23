fd5dbc324bc6eef71de335dd5801f1a74daf57d5
Minh Nghia <dankkingmeo2@gmail.com>
2023-02-23 21:39:41 +0700

    add all

--- src/views/App.js
+++ src/views/App.js
@@ -6,47 +6,28 @@ import Foot from "./foot/Foot.js";
 import Shop from "./shop/Shop";
 import Contact from "./contact/Contact.js";
 import CardShop from "./cardShop/CardShop";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 
-  import 'react-toastify/dist/ReactToastify.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter as Router, Routes, Switch, Route, Link } from "react-router-dom";
 import Coundown from "./coundown/Coundown";
 
 function App() {
   return (
     <Router>
-    <Nav/>
-          <Routes>
-          <Route exact path="/home" element={<Home/>}/>
-          <Route exact path="/shop" element={<Shop/>}/>
-          <Route exact path="/contact" element={<Contact/>}/>
-          <Route exact path="/about" element={<About/>}/>
-          <Route exact path="/Fruits-Shop/" element={<Home/>}/>
-          <Route exact path="/getPay" element={<CardShop/>}/>
-          </Routes>
-    
-    <Foot/>
-    <ToastContainer
-        position="top-center"
-        autoClose={1000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-    />
+      <Nav />
+      <Routes>
+        <Route exact path="/Fruits-Shop/home" element={<Home />} />
+        <Route exact path="/Fruits-Shop/shop" element={<Shop />} />
+        <Route exact path="/Fruits-Shop/contact" element={<Contact />} />
+        <Route exact path="/Fruits-Shop/about" element={<About />} />
+        <Route exact path="/Fruits-Shop" element={<Home />} />
+        <Route exact path="/Fruits-Shop/getPay" element={<CardShop />} />
+      </Routes>
+
+      <Foot />
+      <ToastContainer position="top-center" autoClose={1000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="colored" />
     </Router>
-    
-    
   );
 }
 
